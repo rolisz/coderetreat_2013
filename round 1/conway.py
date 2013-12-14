794c386dda952648fb39f70ae3cebde129edd701
Roland <rolisz@yahoo.com>
2013-12-14 23:02:00 +0200

    Revert Round 1

--- round 1/conway.py
+++ round 1/conway.py
@@ -1,5 +1,71 @@
+from time import sleep
+
 __author__ = 'Roland'
 
 
+def neighbor_count(cell, cells):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            neighbor_cell = (cell[0] + i, cell[1] + j)
+            if neighbor_cell != cell and neighbor_cell in cells:
+                count += 1
+    return count
+
+
+def stays_alive(cell, cells):
+    nr_neighbors = neighbor_count(cell, cells)
+    if 2 <= nr_neighbors <= 3:
+        return True
+    return False
+
+
+def resurect(candidates, cells):
+    resurects = set()
+    for candidate in candidates:
+        if neighbor_count(candidate, cells) == 3:
+            resurects.add(candidate)
+
+    return resurects
+
+
 def tick(cells):
-    new_cells = }
+    new_cells = set()
+
+    for cell in cells:
+        if stays_alive(cell, cells):
+            new_cells.add(cell)
+
+    candidates = set()
+
+    for cell in cells:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                neighbor_cell = (cell[0] + i, cell[1] + j)
+                if neighbor_cell not in cells:
+                    candidates.add(neighbor_cell)
+
+    new_cells.update(resurect(candidates, cells))
+
+    return new_cells
+
+
+def show_universe(cells):
+    min_x = min(map(lambda x: x[0], cells))
+    max_x = max(map(lambda x: x[0], cells))
+    min_y = min(map(lambda x: x[1], cells))
+    max_y = max(map(lambda x: x[1], cells))
+
+    for i in range(min_x, max_x+1):
+        for j in range(min_y, max_y+1):
+            print(" X " if (i, j) in cells else "   ", end="")
+
+        print("\n")
+
+cells = {(0,0) ,(1,1), (2,2), (3,1), (4,1), (4, 0), (3, 3), (3, 4), (4,3), (4,4) }
+
+while True:
+    show_universe(cells)
+    sleep(1)
+    cells =tick(cells)
+    print("===========")
--- round 1/test.py
+++ round 1/test.py
@@ -1,15 +1,34 @@
-from conway import tick
+from conway import *
 import unittest
 
 __author__ = 'Roland'
 
 
 class Test(unittest.TestCase):
-
     def setUp(self):
         pass
 
     def test_two_cells_die(self):
-        cells = [(0,0), (0,1)]
+        cells = {(0, 0), (0, 1)}
         next_cells = tick(cells)
-        self.assertListEqual(next_cells, [])
+        self.assertSetEqual(next_cells, set())
+
+    def test_three_cells_live(self):
+        cells = {(0, 0), (0, 1), (1,1)}
+        next_cells = tick(cells)
+        self.assertSetEqual(next_cells, {(0, 0), (0, 1), (1,1), (1,0)})
+
+    def test_four_neighbors_die(self):
+        cells = {(0,0), (0,1), (1,1), (1,0), (-1,1)}
+        next_cells = tick(cells)
+        self.assertSetEqual(next_cells, {(1, 0), (1,1), (-1,1), (0, 2), (-1, 0)})
+
+
+    def test_neighborhood_count(self):
+        cells = {(0,0) ,(1,1), (2,2), (3,1), (4,1), (4, 0), (3, 3), (3, 4), (4,3), (4,4) }
+        self.assertEquals(1, neighbor_count((0,0), cells))
+        self.assertEquals(2, neighbor_count((1,1), cells))
+        self.assertEquals(3, neighbor_count((2,2), cells))
+        self.assertEquals(3, neighbor_count((3,1), cells))
+        self.assertEquals(4, neighbor_count((3,3), cells))
+        self.assertEquals(0, neighbor_count((100,100), cells))
